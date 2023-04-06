a51fb133bbb710aa4cbff60c779e05911fe93ffb
Cadence <77511497+CadenceElaina@users.noreply.github.com>
2023-04-06 09:21:41 -0400

    part3c - connect backend to db

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
 
+const Note = require('./models/note')
 
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
@@ -21,77 +23,34 @@ app.use(requestLogger)
 app.use(express.static('build'))
 
 let notes = [
-    {
-        id: 1,
-        content: "HTML is easy",
-        important: true
-    },
-    {
-        id: 2,
-        content: "Browser can execute only JavaScript",
-        important: false
-    },
-    {
-        id: 3,
-        content: "GET and POST are the most important methods of HTTP protocol",
-        important: true
-    }
 ]
 
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-})
-
 app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    Note.find({}).then(notes => {
+        response.json(notes)
+    })
 })
-
-const generateId = () => {
-    const maxId = notes.length > 0
-        //note ... spread is necessary for the .map to return indv numbers instead of [1, 2, 3]
-        //so that the Math.max() can evaluate the indv numbers and actually work
-        ? Math.max(...notes.map(n => n.id))
-        : 0
-    return maxId + 1
-}
-
 app.post('/api/notes', (request, response) => {
     const body = request.body
 
-    if (!body.content) {
-        return response.status(400).json({
-            error: 'content missing'
-        })
+    if (body.content === undefined) {
+        return response.status(400).json({ error: 'content missing' })
     }
 
-    const note = {
+    const note = new Note({
         content: body.content,
         important: body.important || false,
-        date: new Date(),
-        id: generateId(),
-    }
+    })
 
-    notes = notes.concat(note)
-
-    response.json(note)
+    note.save().then(savedNote => {
+        response.json(savedNote)
+    })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-    /*  const note = notes.find(note => {
-         console.log(note.id, typeof note.id, id, typeof id, note.id === id)
-         return note.id === id
-     }) */
-
-    if (note) {
+    Note.findById(request.params.id).then(note => {
         response.json(note)
-    } else {
-        //if the id of the note does not exist we return 404 not found
-        response.status(404).end()
-    }
-
-    response.json(note)
+    })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -101,17 +60,9 @@ app.delete('/api/notes/:id', (request, response) => {
     response.status(204).end()
 })
 
-/* app.post('/api/notes', (request, response) => {
-    const note = request.body
-    console.log(note)
-    response.json(note)
-})
- */
-
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
-
